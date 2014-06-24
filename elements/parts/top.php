381891ee466f5db1879b8284fe8bee5d32b0fe90
kkortes <kim@kortes.se>
2014-06-24 14:05:24 +0200

    added livelink to crow

--- elements/parts/top.php
+++ elements/parts/top.php
@@ -4,9 +4,14 @@
       <div class="crow">
         <div class="ws-12">
         	<div class="clearfix">
-
+            <?
+            $crowurl = $app->url.'crow';
+            if($app->environment == 'live') {
+              $crowurl = 'http://getcrow.eu';
+            }
+            ?>
 	        	<div class="pull-right">
-              Based on <a href="<?=$app->url?>crow" class="icon icon-crow"></a> and <a href="http://getbootstrap.com/" target="_blank">Twitter bootstrap</a>
+              Based on <a href="<?=$crowurl?>" class="icon icon-crow"></a> and <a href="http://getbootstrap.com/" target="_blank">Twitter bootstrap</a>
             </div>
 
 	          <a href="<?=$app->url?>" class="link">{ <?=__('top')?> }</a>
