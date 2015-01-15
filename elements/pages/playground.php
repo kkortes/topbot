2f098dc6caf9b701c6dce643ec1b29c09dbdaf87
kkortes <kim@kortes.se>
2015-01-15 17:15:33 +0100

    Changed some pages

---
+++ elements/pages/playground.php
@@ -0,0 +1,20 @@
+<div class="container">
+	<h1>ASksndgjndsg</h1>
+	<p>J asknkgasgk nasgna skkdg ansj hkjasn hkasnlkh asnd hans
+	</p>
+	<h2>ASksndgjndsg</h2>
+	<p>J asknkgasgk nasgna skkdg ansj hkjasn hkasnlkh asnd hans
+	</p>
+	<h3>ASksndgjndsg</h3>
+	<p>J asknkgasgk nasgna skkdg ansj hkjasn hkasnlkh asnd hans
+	</p>
+	<h4>ASksndgjndsg</h4>
+	<p>J asknkgasgk nasgna skkdg ansj hkjasn hkasnlkh asnd hans
+	</p>
+	<h5>ASksndgjndsg</h5>
+	<p>J asknkgasgk nasgna skkdg ansj hkjasn hkasnlkh asnd hans
+	</p>
+	<h6>ASksndgjndsg</h6>
+	<p>J asknkgasgk nasgna skkdg ansj hkjasn hkasnlkh asnd hans
+	</p>
+</div>
